029c4d355dc0f5b1e847ca3b4ae22c0bc9c25344
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-05-01 14:25:35 -0300

    Bump version -> 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='datacatalog-tag-manager',
-    version='0.2.1',
+    version='0.2.2',
     author='Ricardo Mendes',
     author_email='ricardolsmendes@gmail.com',
     description='A package to manage Google Cloud Data Catalog tags,'
