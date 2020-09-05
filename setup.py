3ebe9c6a1cbf4353fb3ac5d114659c37e8339308
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-09-04 22:23:51 -0300

    Bump version: 0.2.4 → 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='datacatalog-tag-manager',
-    version='0.2.4',
+    version='1.0.0',
     url='https://github.com/ricardolsmendes/datacatalog-tag-manager',
     author='Ricardo Mendes',
     author_email='ricardolsmendes@gmail.com',
