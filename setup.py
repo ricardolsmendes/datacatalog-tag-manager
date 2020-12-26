4c63d516f6cd348592dc893d23169806386065d3
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-12-26 16:18:58 -0300

    Add the Python 3.8 compatibility classifier

--- setup.py
+++ setup.py
@@ -34,5 +34,6 @@ setuptools.setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
