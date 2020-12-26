600faa3d3e9ef9cf06ed94d3e8d312707a6805ee
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-12-26 17:19:08 -0300

    Prepare the 2.2.0 release

--- tests/datacatalog_tag_manager/tag_manager_cli_test.py
+++ tests/datacatalog_tag_manager/tag_manager_cli_test.py
@@ -6,6 +6,31 @@ from datacatalog_tag_manager import tag_manager_cli
 
 
 class TagManagerCLITest(unittest.TestCase):
+    __CLI_MODULE = 'datacatalog_tag_manager.tag_manager_cli'
+    __CLI_CLASS = f'{__CLI_MODULE}.TagManagerCLI'
+
+    @mock.patch(f'{__CLI_CLASS}._parse_args')
+    def test_run_should_parse_args(self, mock_parse_args):
+        tag_manager_cli.TagManagerCLI.run([])
+        mock_parse_args.assert_called_once()
+
+    @mock.patch(f'{__CLI_CLASS}._TagManagerCLI__upsert_tags')
+    @mock.patch(f'{__CLI_CLASS}._parse_args')
+    def test_run_upsert_tags_should_call_upsert_tags(self, mock_parse_args, mock_upsert_tags):
+        mock_parse_args.return_value.func = mock_upsert_tags
+        tag_manager_cli.TagManagerCLI.run([])
+        mock_upsert_tags.assert_called_once_with(mock_parse_args.return_value)
+
+    @mock.patch(f'{__CLI_CLASS}._TagManagerCLI__delete_tags')
+    @mock.patch(f'{__CLI_CLASS}._parse_args')
+    def test_run_delete_tags_should_call_delete_tags(self, mock_parse_args, mock_delete_tags):
+        mock_parse_args.return_value.func = mock_delete_tags
+        tag_manager_cli.TagManagerCLI.run([])
+        mock_delete_tags.assert_called_once_with(mock_parse_args.return_value)
+
+    def test_parse_args_no_subcommand_should_raise_system_exit(self):
+        self.assertRaises(SystemExit, tag_manager_cli.TagManagerCLI._parse_args,
+                          ['--csv-file', 'test.csv'])
 
     def test_parse_args_invalid_subcommand_should_raise_system_exit(self):
         self.assertRaises(SystemExit, tag_manager_cli.TagManagerCLI._parse_args, ['create'])
@@ -17,28 +42,38 @@ class TagManagerCLITest(unittest.TestCase):
         args = tag_manager_cli.TagManagerCLI._parse_args(['upsert', '--csv-file', 'test.csv'])
         self.assertEqual('test.csv', args.csv_file)
 
-    def test_run_no_args_should_raise_attribute_error(self):
-        self.assertRaises(AttributeError, tag_manager_cli.TagManagerCLI.run, None)
+    @mock.patch(f'{__CLI_CLASS}._TagManagerCLI__upsert_tags')
+    def test_parse_args_upsert_tags_should_set_default_function(self, mock_upsert_tags):
+        args = tag_manager_cli.TagManagerCLI._parse_args(['upsert', '--csv-file', 'test.csv'])
+        self.assertEqual(mock_upsert_tags, args.func)
 
-    @mock.patch(
-        'datacatalog_tag_manager.tag_manager_cli.tag_datasource_processor.TagDatasourceProcessor')
-    def test_run_upsert_tags_should_call_processor_upsert(self, mock_tag_datasource_processor):
+    def test_parse_args_delete_tags_missing_mandatory_args_should_raise_system_exit(self):
+        self.assertRaises(SystemExit, tag_manager_cli.TagManagerCLI._parse_args, ['delete'])
+
+    def test_parse_args_delete_tags_should_parse_mandatory_args(self):
+        args = tag_manager_cli.TagManagerCLI._parse_args(['delete', '--csv-file', 'test.csv'])
+        self.assertEqual('test.csv', args.csv_file)
+
+    @mock.patch(f'{__CLI_CLASS}._TagManagerCLI__delete_tags')
+    def test_parse_args_delete_tags_should_set_default_function(self, mock_delete_tags):
+        args = tag_manager_cli.TagManagerCLI._parse_args(['delete', '--csv-file', 'test.csv'])
+        self.assertEqual(mock_delete_tags, args.func)
+
+    @mock.patch(f'{__CLI_MODULE}.tag_datasource_processor.TagDatasourceProcessor')
+    def test_upsert_tags_should_upsert_tags_from_csv(self, mock_tag_datasource_processor):
         tag_manager_cli.TagManagerCLI.run(['upsert', '--csv-file', 'test.csv'])
+        mock_tag_datasource_processor.assert_called_with()
+        mock_tag_datasource_processor.return_value.upsert_tags_from_csv.assert_called_with(
+            file_path='test.csv')
 
-        tag_datasource_processor = mock_tag_datasource_processor.return_value
-        tag_datasource_processor.upsert_tags_from_csv.assert_called_once()
-        tag_datasource_processor.upsert_tags_from_csv.assert_called_with(file_path='test.csv')
-
-    @mock.patch(
-        'datacatalog_tag_manager.tag_manager_cli.tag_datasource_processor.TagDatasourceProcessor')
-    def test_run_delete_tags_should_call_processor_delete(self, mock_tag_datasource_processor):
+    @mock.patch(f'{__CLI_MODULE}.tag_datasource_processor.TagDatasourceProcessor')
+    def test_delete_tags_should_delete_tags_from_csv(self, mock_tag_datasource_processor):
         tag_manager_cli.TagManagerCLI.run(['delete', '--csv-file', 'test.csv'])
+        mock_tag_datasource_processor.assert_called_with()
+        mock_tag_datasource_processor.return_value.delete_tags_from_csv.assert_called_with(
+            file_path='test.csv')
 
-        tag_datasource_processor = mock_tag_datasource_processor.return_value
-        tag_datasource_processor.delete_tags_from_csv.assert_called_once()
-        tag_datasource_processor.delete_tags_from_csv.assert_called_with(file_path='test.csv')
-
-    @mock.patch('datacatalog_tag_manager.tag_manager_cli.TagManagerCLI')
-    def test_main_should_call_cli_run(self, mock_cli):
+    @mock.patch(f'{__CLI_CLASS}.run')
+    def test_main_should_call_cli_run(self, mock_run):
         datacatalog_tag_manager.main()
-        mock_cli.run.assert_called_once()
+        mock_run.assert_called_once()
